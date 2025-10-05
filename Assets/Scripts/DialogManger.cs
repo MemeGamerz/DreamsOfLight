767fd46d067a4d88981a043a754e705684067c57
Rijul Kumar <rijulk53@gmail.com>
2025-10-05 13:13:53 +0530

    Enhanced the UI and added level 2

--- Assets/Scripts/DialogManger.cs
+++ Assets/Scripts/DialogManger.cs
@@ -1,35 +1,74 @@
 using UnityEngine;
 using TMPro;
 using System;
+using System.Collections;
+using UnityEngine.InputSystem;
 
 public class DialogManager : MonoBehaviour
 {
     public static DialogManager instance;
-
     public GameObject dialogPanel;
     public TextMeshProUGUI dialogText;
+    public GameObject rightClickPrompt;
+    public float clickBufferTime = 0.5f;
+    public float pulseSpeed = 5f;
+    public float pulseAmplitude = 0.05f;
 
     private Action onDialogFinished;
+    private float timeDialogOpened;
+    private Coroutine promptAnimationCoroutine;
 
     void Awake()
     {
-        if (instance == null) instance = this;
-        else Destroy(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void ShowDialog(string text, Action onFinishedCallback)
     {
         dialogPanel.SetActive(true);
+        if (rightClickPrompt != null) rightClickPrompt.SetActive(true);
+
         dialogText.text = text;
         onDialogFinished = onFinishedCallback;
+        timeDialogOpened = Time.time;
+
+        if (promptAnimationCoroutine != null) StopCoroutine(promptAnimationCoroutine);
+        promptAnimationCoroutine = StartCoroutine(AnimatePrompt());
     }
 
     void Update()
     {
-        if (dialogPanel.activeInHierarchy && Input.GetMouseButtonDown(0))
+        if (!dialogPanel.activeInHierarchy) return;
+
+        if (Mouse.current.rightButton.wasPressedThisFrame && Time.time > timeDialogOpened + clickBufferTime)
         {
+            if (promptAnimationCoroutine != null) StopCoroutine(promptAnimationCoroutine);
             dialogPanel.SetActive(false);
+            if (rightClickPrompt != null) rightClickPrompt.SetActive(false);
+
             onDialogFinished?.Invoke();
+        }
+    }
+
+    private IEnumerator AnimatePrompt()
+    {
+        if (rightClickPrompt == null) yield break;
+
+        Vector3 baseScale = rightClickPrompt.transform.localScale;
+
+        while (true)
+        {
+            float scaleOffset = 1.0f + (Mathf.Sin(Time.time * pulseSpeed) * pulseAmplitude);
+            rightClickPrompt.transform.localScale = baseScale * scaleOffset;
+            yield return null;
         }
     }
 }
